d1d4eaa628a987e9f144a2fd725fe3d8b644c3e8
pragn <pragn@DESKTOP-QCPOIJ8>
2020-05-08 22:20:37 +0530

    final

---
+++ cloud-config/src/main/java/com/example/demo/CloudConfigApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
+@EnableConfigServer
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
+public class CloudConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudConfigApplication.class, args);
+	}
+
+}
